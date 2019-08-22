53eaeed6b1009da14cd78d2b77d332690bf2d6d4
Xiaoxue <e0323328@u.nus.edu>
2019-08-22 14:06:42 +0800

    Level-2:Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Duke {
+    /* Level 1:Greet,Echo,Exit
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -14,6 +15,32 @@ public class Duke {
             }
             else {
                 System.out.println("  "+echo);
+            }
+        }
+    }*/
+
+    //Level 2:Add,List
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        String list[] = new String[100];
+        int count = 0;
+        while (true){
+            Scanner input = new Scanner(System.in);
+            String command = input.nextLine();
+            if(command.equals("bye")){
+                System.out.println("  Bye. Hope to see you again soon!");
+                break;
+            }
+            else if(command.equals("list")){
+               for(int i = 0; i < count; i++){
+                   System.out.println("  "+(i+1)+". "+list[i]);
+               }
+            }
+            else{
+                list[count] = command;
+                System.out.println("  added: "+command);
+                count ++;
             }
         }
     }
